32567bd15152dbc71935dcc1a17809e20c431995
Richard Waller <richard.lh.waller@gmail.com>
2021-12-06 12:57:35 +0000

    Add favicon & other icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import Image from 'next/image';
+import logo from '../../public/apple-touch-icon.png';
 
 export const Header: React.VFC = () => (
   <header className="sticky top-0 z-50 px-2 py-2 bg-white border-b">
+    <div className="flex items-center justify-center space-x-1">
+      <div className="font-semibold text-center text-m ">Time to Leave</div>
+      <div className="relative flex-shrink-0 w-8 h-8 sm:table-cell">
+        <Image src={logo} alt={'Logo'} layout="fill" />
+      </div>
+    </div>
   </header>
 );
