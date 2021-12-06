f3bada2724932148c6c5cc37b598079c13c62721
Richard Waller <richard.lh.waller@gmail.com>
2021-12-06 18:00:12 +0000

    Search departure times more strategically (every 30 mins from 11pm)

--- src/services/fetchJourneys.ts
+++ src/services/fetchJourneys.ts
@@ -3,26 +3,47 @@ import mockGoogleResponses from './mockGoogleResponses';
 import { Location } from '../pages/index';
 import { reallyCallGoogleAPI } from '../config';
 
-const getGoogleResponses = async (origin: Location, destination: Location) => {
+const getDepartureTimes = () => {
+  const MILLISECONDS_PER_HOUR = 60 * 60 * 1000;
+
+  const MILLISECONDS_PER_DAY = MILLISECONDS_PER_HOUR * 24;
+
+  const ELEVEN_PM = 23;
+
+  const isPast6AM = new Date().getHours() > 5;
+
+  const firstDepartureDate = new Date();
+  firstDepartureDate.setHours(ELEVEN_PM);
+  firstDepartureDate.setMinutes(0);
+  firstDepartureDate.setSeconds(0);
+  firstDepartureDate.setMilliseconds(0);
+
+  const firstDepartureTime = isPast6AM
+    ? Number(firstDepartureDate)
+    : Number(firstDepartureDate) - MILLISECONDS_PER_DAY;
+
+  const departureTimes = [
+    new Date(Number(firstDepartureTime) + 0 * MILLISECONDS_PER_HOUR),
+    new Date(Number(firstDepartureTime) + 0.5 * MILLISECONDS_PER_HOUR),
+    new Date(Number(firstDepartureTime) + 1 * MILLISECONDS_PER_HOUR),
+    new Date(Number(firstDepartureTime) + 1.5 * MILLISECONDS_PER_HOUR),
+    new Date(Number(firstDepartureTime) + 2 * MILLISECONDS_PER_HOUR),
+    new Date(Number(firstDepartureTime) + 2.5 * MILLISECONDS_PER_HOUR),
+    new Date(Number(firstDepartureTime) + 3 * MILLISECONDS_PER_HOUR),
+    new Date(Number(firstDepartureTime) + 3.5 * MILLISECONDS_PER_HOUR),
+    new Date(Number(firstDepartureTime) + 4 * MILLISECONDS_PER_HOUR),
+  ];
+  return departureTimes;
+};
+
+const getGoogleResponses = async (
+  origin: Location,
+  destination: Location,
+  departureTimes: Date[]
+) => {
   const directionsService = new google.maps.DirectionsService();
 
   const travelMode = google.maps.TravelMode.TRANSIT;
-
-  const MILLISECONDS_PER_HOUR = 60 * 60 * 1000;
-
-  const now = Date.now();
-
-  const departureTimes = [
-    new Date(now + 0 * MILLISECONDS_PER_HOUR),
-    new Date(now + 1 * MILLISECONDS_PER_HOUR),
-    new Date(now + 2 * MILLISECONDS_PER_HOUR),
-    new Date(now + 3 * MILLISECONDS_PER_HOUR),
-    new Date(now + 4 * MILLISECONDS_PER_HOUR),
-    new Date(now + 5 * MILLISECONDS_PER_HOUR),
-    new Date(now + 6 * MILLISECONDS_PER_HOUR),
-    new Date(now + 7 * MILLISECONDS_PER_HOUR),
-    new Date(now + 8 * MILLISECONDS_PER_HOUR),
-  ];
 
   const responses = await Promise.all(
     departureTimes.map(async (departureTime) => {
@@ -48,8 +69,10 @@ export const fetchJourneys = async (
   origin: Location,
   destination: Location
 ) => {
+  const departureTimes = getDepartureTimes();
+
   const responses = reallyCallGoogleAPI
-    ? await getGoogleResponses(origin, destination)
+    ? await getGoogleResponses(origin, destination, departureTimes)
     : mockGoogleResponses;
 
   return extractJourneyInfo(responses);
