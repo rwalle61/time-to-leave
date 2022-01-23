b622e94640f2cd22401ce1e783bef0dcb9ba5f30
Richard Waller <richard.lh.waller@gmail.com>
2022-01-23 19:46:07 +0000

    Add refresh button on error page (for iOS Safari bookmark shortcut)

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -5,8 +5,17 @@ import {
 } from 'react-error-boundary';
 
 const ErrorFallback = ({ error }: FallbackProps): ReactElement => (
-  <div className="container px-2 py-2 mx-auto italic text-red-700">
-    <p className="text-center">{`You found a bug! 🥳 Please refresh`}</p>
+  <div className="container flex flex-col px-2 py-2 mx-auto italic text-red-700 align-middle space-y-2">
+    <p className="text-center">You found a bug! 🥳</p>
+    <button
+      type="button"
+      className="px-2 py-1 mx-auto text-xs text-white bg-purple-400 rounded-xl hover:bg-black hover:text-white hover:border-transparent"
+      onClick={() => {
+        window.location.reload();
+      }}
+    >
+      Refresh
+    </button>
     <pre className="text-xs text-left whitespace-pre-wrap">{`${error.message}\n ${error.stack}`}</pre>
   </div>
 );
